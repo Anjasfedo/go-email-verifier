04381819e4b251ba5194b6f99c9a90d63a0c0be5
AnjasFedo <fedoaf98@gmail.com>
2024-01-14 20:57:13 +0700

    Define main Function

--- main.go
+++ main.go
@@ -10,9 +10,26 @@ import (
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 
+	fmt.Printf("domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Can't Read From Input: %v", err)
+	}
 }
 
 func checkDomain(domain string) {
+	var hasMX, hasSPF, hasDMARC bool
 
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
 }
